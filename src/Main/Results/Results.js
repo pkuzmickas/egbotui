5bd28e4f055efc0c5df9538cdca29afde7d8e3bb
Paulius Kuzmickas <p.kuzmickas@gmail.com>
2019-04-02 13:04:05 +0100

    Binded to backend

--- src/Main/Results/Results.js
+++ src/Main/Results/Results.js
@@ -3,15 +3,31 @@ import "../../index";
 import "./Results.css";
 import { ok } from "assert";
 import { Link, withRouter } from "react-router-dom";
+import { browserHistory } from "react-router-dom";
 
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answ: null
+    };
+    let topic = this.formatUrl();
+    this.getAnswer(topic);
+    this.props.history.listen((location, action) => {
+      topic = this.formatUrl(location.pathname);
+      this.getAnswer(topic);
+    });
+  }
+
   handleClick = event => {
     // accessible
-    if(event.target.classList[0]=="positive") {
-        event.target.style.color = event.target.style.color == "green" ? "black" : "green"
+    if (event.target.classList[0] == "positive") {
+      event.target.style.color =
+        event.target.style.color == "green" ? "black" : "green";
     }
-    if(event.target.classList[0]=="negative") {
-        event.target.style.color = event.target.style.color == "red" ? "black" : "red"
+    if (event.target.classList[0] == "negative") {
+      event.target.style.color =
+        event.target.style.color == "red" ? "black" : "red";
     }
   };
   example = (
@@ -28,49 +44,34 @@ class Results extends Component {
         <span onClick={this.handleClick} class="positive thumbs fa">
           &#xf087;
         </span>{" "}
-        <span onClick={this.handleClick} class="negative thumbs fa">&#xf088;</span>
+        <span onClick={this.handleClick} class="negative thumbs fa">
+          &#xf088;
+        </span>
       </div>
       <hr class="info" />
     </div>
   );
 
-  examples = [
-    <div>
-      <p class="info">
-        <span class="bigger">Examples</span>
-        <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation
-        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        <br />
-      </p>
-      <div class="feedback">
-        <i>Was this example helpful?</i>{" "}
-        <span onClick={this.handleClick} class="positive thumbs fa">
-          &#xf087;
-        </span>{" "}
-        <span onClick={this.handleClick} class="negative thumbs fa">&#xf088;</span>
-      </div>
-      <hr class="info" />
-    </div>
-  ];
+  examples = [];
 
-  generate = () => {
-    this.examples.push(this.example);
-  };
-
-  showExamples = () => {
+  showRelated = relatedList => {
     let list = [];
-    for (let i = 0; i < this.examples.length; i++) {
-      list.push(this.examples[i]);
+    for (let i = 0; i < relatedList.length; i++) {
+      list.push(
+        <div class="related gray-border blue-background" id="examples">
+          <p class="info">{relatedList[i].title}</p>
+        </div>
+      );
     }
     return list;
   };
 
-  formatUrl() {
-    let url = this.props.location.pathname.split("/");
+  formatUrl(url = null) {
+    if (!url) {
+      url = this.props.location.pathname.split("/");
+    } else {
+      url = url.split("/");
+    }
     if (url.length == 3) return url[2];
     else if (url.length == 4) {
       return url[3] + " " + url[2];
@@ -79,38 +80,74 @@ class Results extends Component {
     }
   }
 
+  getAnswer(query) {
+    let url = "https://egbot.good-loop.com/ask?q=" + query;
+    console.log(url);
+    return fetch(url)
+      .then(resp => resp.json())
+      .then(rjson => {
+        this.setState({
+          answ: rjson
+        });
+        console.log(this.state.answ);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
+    let related;
+    let example;
+    if (!this.state.answ) {
+      related = <div />;
+      example = <div />;
+    } else {
+      related = this.showRelated(this.state.answ.data.relatedQs);
+      example = this.state.answ.data.generatedAnswer;
+    }
     return (
-      <div class="row">
-        <div id="response">
-          <div class="example">
-            <p class="info">
-              <span class="bigger">Viewing results for: </span>{" "}
-              {this.formatUrl()}
-            </p>
-            <hr />
-            <p class="info">
-              <span class="bigger">Explanation</span>
-              <br /> Lorem ipsum dolor sit amet,{" "}
-              <Link to="/results/consectetur">consectetur</Link> adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea{" "}
-              <Link to="/results/commodo%20consequat">commodo consequat</Link>.
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-              <br />
-              <b>External links:</b> <a href="#">link1</a>,{" "}
-              <a href="#">link2</a>, <a href="#">link3</a>
-            </p>
-          </div>
-          <div class="example gray-border blue-background" id="examples">
-            {this.showExamples()}
+      <div>
+        <div class="row">
+          <div id="response">
+            <div class="example">
+              <p class="info no-mg-top">
+                <span class="bigger">Viewing results for: </span>{" "}
+                {this.formatUrl()}
+              </p>
+              <hr />
+              <p class="info">
+                <span class="bigger">Examples </span>{" "}
+              </p>
+            </div>
+            <div class="example gray-border greenish" id="examples">
+              <div>
+                <p class="info">
+                  {example}
+                  <br />
+                </p>
+                <div class="feedback">
+                  <i>Was this example helpful?</i>{" "}
+                  <span onClick={this.handleClick} class="positive thumbs fa">
+                    &#xf087;
+                  </span>{" "}
+                  <span onClick={this.handleClick} class="negative thumbs fa">
+                    &#xf088;
+                  </span>
+                </div>
+                {/* <hr class="info" /> */}
+              </div>
+            </div>
+
+            <div class="example">
+              <p class="info">
+                <span class="bigger">Related queries </span>{" "}
+              </p>
+              {related}
+            </div>
           </div>
         </div>
-        <button
+        {/* <button
           type="button"
           id="genbutton"
           class="btn btn-success"
@@ -119,9 +156,8 @@ class Results extends Component {
             this.forceUpdate();
           }}
         >
-          {/* NEUZSIUPDATINA CJ REIK KAZKA SU REACT STATE RENDER IDK */}
           GENERATE MORE
-        </button>
+        </button> */}
       </div>
     );
   }
